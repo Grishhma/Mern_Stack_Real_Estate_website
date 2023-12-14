c1381a81e79a3a3093ca7ff26c6030d83682746d
Grishma <karkera.g@northeastern.edu>
2023-12-14 11:40:17 -0500

    Created API folder

---
+++ api/index.js
@@ -0,0 +1,8 @@
+import express from 'express';
+
+const app = express(); //comment
+
+app.listen(3000, () => {
+    console.log('server running on port 3000'); //ignore
+}
+);
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,10 @@
 import {FaSearch} from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'; //ignore
 
 export default function Header() {
   return (
     <header className='bg-slate-200 shadow-md'>
-     <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+     <div className='flex justify-between items-center max-w-6xl mx-auto p-3'> 
         
         <Link to='/'>
         <h1 className='font-bold text-sm:text-xl flex-wrap'>
